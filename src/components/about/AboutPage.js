6aa5e59b778e66c64b23156ffd8fad2d080ed5b1
biznisprofil <biznis.profil@hotmail.com>
2019-04-15 23:11:40 +0200

    first commit

---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const AboutPage = () => {
+  return (
+    <div>
+      <h2>About</h2>
+      <p>
+        Thiss app uses React, Redux, React Router and many other helpfuly
+        libraries.
+      </p>
+    </div>
+  );
+};
+
+export default AboutPage;
---
+++ src/components/courses/CoursesPage.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class CoursesPage extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <h1>Courses</h1>
+      </div>
+    );
+  }
+}
+
+export default CoursesPage;
